2c1145f58c6b42265781e25395fc56c6f050883c
Webbie253 <mjhiggins2@loyola.edu>
2021-11-07 20:02:10 -0500

    dynamically added option Values

--- thisfile.js
+++ thisfile.js
@@ -1,11 +1,39 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var div = document.createElement('div');
-    var doc = document.createElement('select');
-    doc.id="animals";
-    div.id="wrap";
-    doc.ATTRIBUTE_NODE = div;
-    document.body.appendChild(div).appendChild(doc);
-    doc.innerHTML=doc.id;
+    var part_1 = document.createElement('div');
+    var part_2 = document.createElement('select');
+    
+    let newOption = new Option('Giraffe','other');
+    part_2.add(newOption,undefined)
+
+    let newOption2 = new Option('Greyhound','other');
+    part_2.add(newOption2,undefined)
+
+    let newOption3 = new Option('Zebra','other');
+    part_2.add(newOption3,undefined)
+
+    let newOption4 = new Option('Elephant','other');
+    part_2.add(newOption4,undefined)
+
+    let newOption5 = new Option('Squirrel','other');
+    part_2.add(newOption5,undefined)
+
+    let newOption6 = new Option('Bird','other');
+    part_2.add(newOption6,undefined)
+
+    let newOption7 = new Option('Cat','other');
+    part_2.add(newOption7,undefined)
+
+    let newOption8 = new Option('Deer','other');
+    part_2.add(newOption8,undefined)
+
+    let newOption9 = new Option('Hyena','other');
+    part_2.add(newOption9,undefined)
+
+    let newOption10 = new Option('Bear','other');
+    part_2.add(newOption10,undefined)
+
+    part_1.add(part_2)
+    document.body.appendChild(part_1);
 }, false);
 
 
