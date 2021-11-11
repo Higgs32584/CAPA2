2f8ee422fe33179a92c5c117ad149bf0a74e1ed4
Webbie253 <mjhiggins2@loyola.edu>
2021-11-11 14:20:10 -0500

    GOT THE DIV TO WORK!!! YAY!!

--- thisfile.js
+++ thisfile.js
@@ -1,63 +1,61 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var part_1 = document.createElement('div');
 
-    var part_2 = document.createElement('select');
+var part_1 = document.createElement('div');
+var part_2 = document.createElement('select');
+var part_3 = document.createElement('p');
 
-    var part_3 = document.createElement('body');
-    //part_2.onselectionchange="part_3.textContent = this is really bad";
-    //document.getElementById("Giraffe").text = "newTextForApple";
+part_1.appendChild(part_2);
 
-    part_3.textContent = "these are some of the values that I plan on changing";
-    
-    let newOption = new Option('Giraffe',value="red");
-    part_2.add(newOption,undefined)
+part_1.id="part_1";
+part_2.id="part_2";
+part_3.id="part_3";
+part_3.innerText="this";
 
-    let newOption2 = new Option('Greyhound',value="red");
-    part_2.add(newOption2,undefined)
+let newOption = new Option('Giraffe',value="red");
+  part_2.add(newOption,undefined)
 
-    let newOption3 = new Option('Zebra',value="red");
-    part_2.add(newOption3,undefined)
+  let newOption2 = new Option('Greyhound',value="red");
+  part_2.add(newOption2,undefined)
 
-    let newOption4 = new Option('Elephant',value="red");
-    part_2.add(newOption4,undefined)
+  let newOption3 = new Option('Zebra',value="red");
+  part_2.add(newOption3,undefined)
 
-    let newOption5 = new Option('Squirrel',value="red");
-    part_2.add(newOption5,undefined)
+  let newOption4 = new Option('Elephant',value="red");
+  part_2.add(newOption4,undefined)
 
-    let newOption6 = new Option('Bird',value="red");
-    part_2.add(newOption6,undefined)
+  let newOption5 = new Option('Squirrel',value="red");
+  part_2.add(newOption5,undefined)
 
-    let newOption7 = new Option('Cat',value="red");
-    part_2.add(newOption7,undefined)
+  let newOption6 = new Option('Bird',value="red"); 
+  part_2.add(newOption6,undefined)
 
-    let newOption8 = new Option('Deer',value="red");
-    part_2.add(newOption8,undefined)
+  let newOption7 = new Option('Cat',value="red");
+  part_2.add(newOption7,undefined)
 
-    let newOption9 = new Option('Hyena','other');
-    part_2.add(newOption9,undefined)
+  let newOption8 = new Option('Deer',value="red");
+  part_2.add(newOption8,undefined)
 
-    let newOption10 = new Option('Bear','other');
-    part_2.add(newOption10,undefined)
+  let newOption9 = new Option('Hyena','other');
+  part_2.add(newOption9,undefined)
 
-    document.body.appendChild(part_1).appendChild(part_2)
-    document.body.appendChild(part_3);
+  let newOption10 = new Option('Bear','other');
+  part_2.add(newOption10,undefined)
+
+  document.body.appendChild(part_1);
+  document.body.appendChild(part_3);
 }, false);
-/**<h>Would you like to see count values per hour or minute?</h><br>
-<Select id="Selector">
-   <option value="hr">Hour</option>
-   <option value="min">Minute</option>
-</Select>
-<script>
-  document.getElementById("Selector").onchange = changeListener;
-  
-  function changeListener(){
-  var value = this.value
-    console.log(value);
-    
-    if (value == "min"){
-      document.body.style.background = "red";
-    }else if (value == "hr"){
-      document.body.style.background = "blue";
-    }
-    
-  }**/
+
+document.addEventListener("change", function() {
+let x = document.getElementById("part_2").selectedIndex
+
+document.getElementById("part_3").innerText="Congrats!! you selected the" + x;
+});
+
+//function state_change() {
+  // var x = document.getElementById("part_2").value;
+  // var y= document.createElement('div');
+  // y.innerHTML="works";
+  // document.body.appendChild(y);
+  // document.getElementById("part_3").innerHTML = "You selected: " + x;
+  //alert("I heard you!!")
+//}
